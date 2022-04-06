efe33027595642b6fac74484a3209ade75b46ab6
Nachiketa Kumar <nachiketa1481@gmail.com>
2022-04-07 01:27:20 +0530

    Adding first java program

---
+++ PracticeClass.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class PracticeClass {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        System.out.println("Hey How are you. you entered " + input);
+    }
+}
+
